9d6606b76b4b0a8163ac7eb317fef75ad9d156f8
Mo <mojaywg@gmail.com>
2020-05-01 20:52:40 +0200

    updated routes and css as we as boot strap

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,33 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about-us', function () {
+    return view('about-us.html');
+});
+
+Route::get('/login', function () {
+    return view('login.html');
+});
+
+Route::get('/contact-us', function () {
+    return view('contact-us.html');
+});
+
+
+Route::get('/registration', function () {
+    return view('registration.html');
+});
+
+Route::get('/features', function () {
+    return view('features.html');
+});
+
+
+Route::get('/pricing', function () {
+    return view('pricing.html');
+});
+
+/*	Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');*/
